359a4ea6800e8a68b34de72c004dbee830d3920f
rigas <sassalosrigas@gmail.com>
2023-11-21 20:08:46 +0200

    finished PrefixToInfix.java (?)

--- PrefixToInfix.java
+++ PrefixToInfix.java
@@ -3,16 +3,14 @@ import java.util.Scanner;
 public class PrefixToInfix {
 
     Scanner sc = new Scanner(System.in);
-
-    System.out.println("Enter a prefix expression: ");
-    String prefic = sc.nextLine();
+    String prefix = sc.nextLine();
 
     public PrefixToInfix(String prefix) {
         this.prefix = prefix;
     }
 
-    static boolean isOperator(String token) {
-        switch(token) {
+    public boolean isOperator(String item) {
+        switch(item) {
             case "+":
             case "-":
             case "*":
@@ -21,6 +19,45 @@ public class PrefixToInfix {
         }
         return false;
     }
+
+    public boolean isValid(String[] prefixArray) {
+        boolean flag = false;
+        if(isOperator(prefixArray[0])) {
+            flag = true;
+        }
+        for (int i = 0; i < prefixArray.length; i++) {
+            if(!prefixArray[i].matches("[0-9]+")) {
+                flag = false;
+            }
+        }   
+        return flag;  
+    }
+
+    public String convert() {
+        String[] prefixArray = prefix.split(" ");
+        if(isValid(prefixArray)) {
+            StringDoubleEndedQueueImpl queue = new StringDoubleEndedQueueImpl();
+            for (int i = 0; i < prefixArray.length; i++) {
+                queue.addLast(prefixArray[i]);
+            }
+            String infix = "";
+            while (!queue.isEmpty()) {
+                String item = queue.removeFirst();
+                if (isOperator(item)) {
+                    String operand1 = queue.removeFirst();
+                    String operand2 = queue.removeFirst();
+                    infix = "(" + operand1 + item + operand2 + ")";
+                    //queue.addFirst(infix);
+                } else {
+                    queue.addLast(item);
+                }
+            }
+            return infix;
+        } else {
+            return "Invalid prefix expression";
+        }
+    }
+
 
 
 }
